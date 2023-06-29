e4e24c9fe40e16d8e88a3241517267b989f82d94
Manoj  Kushwaha <manoj.k@synetalsolutions.com>
2023-06-29 15:07:30 +0530

    design

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,13 +2,14 @@ import React from "react";
 
 const Button = ({ title, icon, event }) => {
   return (
-    <div
+    <button
+      type="button"
       onClick={event}
-      className="bg-primary-button justify-center text-sm flex items-center gap-1 cursor-pointer tracking-wider py-2 px-4 rounded text-white"
+      className="bg-primary-button justify-center text-sm flex items-center gap-1.5 cursor-pointer tracking-wider p-2 rounded text-white"
     >
       {icon}
       {title}
-    </div>
+    </button>
   );
 };
 
